30bdef61d06bb7805981ccf679e6b66c5d4389a5
cody13131ca <iikdi.1226@gmail.com>
2021-10-02 23:19:16 +0900

    Likeされていても記事が投稿できるようにした

--- myapp/models.py
+++ myapp/models.py
@@ -60,19 +60,19 @@ class Like(models.Model):
     post = models.ForeignKey(
         Post,
         verbose_name="投稿",
-        on_delete=models.PROTECT)
+        on_delete=models.CASCADE)
     user = models.ForeignKey(
         User,
         verbose_name="LIKEしたユーザー",
-        on_delete=models.PROTECT)
+        on_delete=models.CASCADE)
 
 
 class DisLike(models.Model):
     post = models.ForeignKey(
         Post,
         verbose_name="投稿",
-        on_delete=models.PROTECT)
+        on_delete=models.CASCADE)
     user = models.ForeignKey(
         User,
         verbose_name="DisLIKEしたユーザー",
-        on_delete=models.PROTECT)
+        on_delete=models.CASCADE)
